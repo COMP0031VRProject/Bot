cf597124a02dd3c43d39397eb995584166ebc91a
unknown <jamzhong@gmail.com>
2021-03-09 17:33:55 +0800

    add json

--- Bot/Assets/Mesh.cs
+++ Bot/Assets/Mesh.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class Mesh
 {
-    public List<(decimal, decimal)> verts;
-    public List<(int, int, int)> tInd;
-    public Mesh(List<(decimal, decimal)> verts, List<(int, int, int)> tInd)
+    public List<List<decimal>> verts;
+    public List<List<int>> tInd;
+
+    public Mesh(List<List<decimal>> verts, List<List<int>> tInd)
     {
         this.verts = verts;
         this.tInd = tInd;
     }
+
 }
--- Bot/Assets/WorldScript.cs
+++ Bot/Assets/WorldScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class WorldScript : MonoBehaviour
 {
-    public Mesh[] virtualMeshs;
-    public Mesh[] realMeshs;
     public Mesh virtualMesh;
     public Mesh realMesh;
+    public TextAsset virtualJson;
+    public TextAsset realJson;
     public Transform virtualbot;
     public Transform realbot;
     public Transform[] flags;
@@ -23,11 +23,12 @@ public class WorldScript : MonoBehaviour
     void Start()
     {
         // Create all meshes for the environment
-        generate_meshes();
+        //generate_meshes();
+        readMeshFiles();
 
     }
 
-    void placeMeshes()
+   /* void placeMeshes()
     {
         Utils util = new Utils();
         for (int i = 0; i < flags.Length; i++)
@@ -36,19 +37,31 @@ public class WorldScript : MonoBehaviour
             Mesh new_mesh = util.generate_embedded_polygon_mesh(20, 5, 2, (0, 0));
 
         }
-    }
+    } */
 
     void FixedUpdate()
     {
-        // Map the virtual bot to real bot 50 frames per second
-        (decimal x, decimal z) = virtual2Real(virtualbot);
-        if (x != 0 | z!= 0)
-        {
-            realbot.position = new Vector3((float)x, 0.5f, (float)z);
-        }
+       // Map the virtual bot to real bot 50 frames per second
+       List<decimal>coordinates  = virtual2Real(virtualbot);
+       if (coordinates != null)
+       {
+           realbot.position = new Vector3((float)coordinates[0], 0.5f, (float)coordinates[1]);
+       }
+       else
+       {
+            Debug.Log("damn");
+       }
     }
 
-    // TODO: Create mesh around each of the 6 flags and apply homogenous to rest of area
+    void readMeshFiles()
+    {
+        // Create meshes from json files
+        realMesh = Newtonsoft.Json.JsonConvert.DeserializeObject<Mesh>(realJson.text);
+        virtualMesh = Newtonsoft.Json.JsonConvert.DeserializeObject<Mesh>(virtualJson.text);
+    }
+
+    
+   /*
     void generate_meshes()
     {
         Utils util = new Utils();
@@ -58,12 +71,12 @@ public class WorldScript : MonoBehaviour
 
 
         // Homogenous or polygon manipulation
-        (decimal, decimal) center = realMesh.verts[0];
+        List<decimal> center = realMesh.verts[0];
 
         for (int i = 0; i < realMesh.verts.Count; i++)
         {
-            decimal x = (realMesh.verts[i].Item1 - center.Item1) / (scale + center.Item1);
-            decimal y = (realMesh.verts[i].Item2 - center.Item2) / (scale + center.Item2);
+            decimal x = (realMesh.verts[i][0] - center[0]) / (scale + center[0]);
+            decimal y = (realMesh.verts[i][1] - center[1]) / (scale + center[1]);
             realMesh.verts[i] = (x, y);
         }
 
@@ -72,98 +85,109 @@ public class WorldScript : MonoBehaviour
             for (int i = 0; i <= n; i++)
             {
                 (decimal x, decimal y) = realMesh.verts[i];
-                realMesh.verts[i] = ((x - center.Item1) * scale + center.Item1, (y - center.Item2) * scale + center.Item2);
+                realMesh.verts[i] = ((x - center[0]) * scale + center[0], (y - center[1]) * scale + center[1]);
             }
         }
         
-    }
+    } */
 
-    (decimal, decimal) real2Virtual(Transform pos)
+    List<decimal> real2Virtual(Transform pos)
     {
 
         Utils util = new Utils();
-        (decimal, decimal) P = ((decimal)pos.position.x, (decimal)pos.position.z);
+        List<decimal> P = new List<decimal> { (decimal)pos.position.x, (decimal)pos.position.z };
         decimal i1, i2;
         //Debug.Log(realMesh.tInd.Count);
-        foreach ((int t1, int t2, int t3) in realMesh.tInd)
+        foreach (List<int> t in realMesh.tInd)
         {
-            (decimal, decimal) A, B, C;
-            A = realMesh.verts[t1];
-            B = realMesh.verts[t2];
-            C = realMesh.verts[t3];
+            List<decimal> A, B, C;
+            A = realMesh.verts[t[0]];
+            B = realMesh.verts[t[1]];
+            C = realMesh.verts[t[2]];
             
 
             if (util.is_point_in_triangle(P, A, B, C))
             {
                 (decimal alpha, decimal beta, decimal gamma) = util.barycentric_coordinates(P, A, B, C);
-                (decimal, decimal) vA, vB, vC;
-                vA = virtualMesh.verts[t1];
-                vB = virtualMesh.verts[t2];
-                vC = virtualMesh.verts[t3];
+                List<decimal> vA, vB, vC;
+                vA = virtualMesh.verts[t[0]];
+                vB = virtualMesh.verts[t[1]];
+                vC = virtualMesh.verts[t[2]];
                 //Debug.Log(vB);
                 //Debug.Log(alpha);
                 //Debug.Log(beta);
                 //Debug.Log(gamma);
 
-                vA.Item1 = vA.Item1 * alpha;
-                vA.Item2 = vA.Item2 * alpha;
-                vB.Item1 = vB.Item1 * beta;
-                vB.Item2 = vB.Item2 * beta;
-                vC.Item1 = vC.Item1 * gamma;
-                vC.Item2 = vC.Item2 * gamma;
+                vA[0] = vA[0] * alpha;
+                vA[1] = vA[1] * alpha;
+                vB[0] = vB[0] * beta;
+                vB[1] = vB[1] * beta;
+                vC[0] = vC[0] * gamma;
+                vC[1] = vC[1] * gamma;
 
                 //Debug.Log("A: " + vA);
                 //Debug.Log("B: " + vB);
                 //Debug.Log("c: " + vC);
 
-                i1 = vA.Item1 + vB.Item1 + vC.Item1;
-                i2 = vA.Item2 + vB.Item2 + vC.Item2;
+                i1 = vA[0] + vB[0] + vC[0];
+                i2 = vA[1] + vB[1] + vC[1];
 
                 //Debug.Log(i1);
-                //Debug.Log(i2);
+                //Debug.Log(i2);  
 
-                return (i1, i2);
+                return new List<decimal> {i1, i2};
             }
 
         }
-        
-        return (0,0);
+
+        return null;
     }
 
-    (decimal, decimal) virtual2Real(Transform pos)
+    List<decimal> virtual2Real(Transform pos)
     {
         Utils util = new Utils();
-        (decimal, decimal) P = ((decimal)pos.position.x, (decimal)pos.position.z);
-        foreach ((int t1, int t2, int t3) in virtualMesh.tInd)
+        List<decimal> P = new List<decimal>{(decimal)pos.position.x, (decimal)pos.position.z};
+
+        foreach (List<decimal> v in virtualMesh.verts)
         {
-            (decimal, decimal) A, B, C;
-            A = virtualMesh.verts[t1];
-            B = virtualMesh.verts[t2];
-            C = virtualMesh.verts[t3];
+            Debug.Log(v[0]);
+        }
+
+        foreach (List<int> t in virtualMesh.tInd)
+        {
+            List<decimal> A, B, C;
+            A = virtualMesh.verts[t[0]];
+            B = virtualMesh.verts[t[1]];
+            C = virtualMesh.verts[t[2]];
 
             if(util.is_point_in_triangle(P, A, B, C))
             {
                 //Debug.Log("IS in triangle " + P);
                 (decimal alpha, decimal beta, decimal gamma) = util.barycentric_coordinates(P, A, B, C);
-                (decimal, decimal) vA, vB, vC;
-                vA = realMesh.verts[t1];
-                vB = realMesh.verts[t2];
-                vC = realMesh.verts[t3];
+                List<decimal> vA, vB, vC;
+                vA = realMesh.verts[t[0]];
+                vB = realMesh.verts[t[1]];
+                vC = realMesh.verts[t[2]];
 
-                vA.Item1 = vA.Item1* alpha;
-                vA.Item2 = vA.Item2 * alpha;
-                vB.Item1 = vB.Item1 * beta;
-                vB.Item2 = vB.Item2 * beta;
-                vC.Item1 = vC.Item1 * gamma;
-                vC.Item2 = vC.Item2 * gamma;
+                vA[0] = vA[0] * alpha;
+                vA[1] = vA[1] * alpha;
+                vB[0] = vB[0] * beta;
+                vB[1] = vB[1] * beta;
+                vC[0] = vC[0] * gamma;
+                vC[1] = vC[1] * gamma;
 
-                decimal i1 = vA.Item1 + vB.Item1 + vC.Item1;
-                decimal i2 = vA.Item2 + vB.Item2 + vC.Item2;
-               
-                return (i1, i2);
+                decimal i1 = vA[0] + vB[0] + vC[0];
+                decimal i2 = vA[1] + vB[1] + vC[1];
+
+                Debug.Log(i1);
+                Debug.Log(i2);
+
+
+
+                return new List<decimal> { i1, i2 };
             }
         }
-        return (0,0);
-    }
+        return null;
+    } 
 
 }
